107a52441e3724ad3fb7b67342d1cef303d634d8
xXDarkayTberXx <madalolito22@gmail.com>
2024-12-18 12:44:12 +0100

    StartCommit

---
+++ src/main/java/com/ar/springboot/proyectofinal/app/Controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.ar.springboot.proyectofinal.app.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+    
+    
+    
+}
---
+++ src/main/java/com/ar/springboot/proyectofinal/app/Model/Product.java
@@ -0,0 +1,9 @@
+package com.ar.springboot.proyectofinal.app.Model;
+
+public class Product {
+   
+    private Long id;
+    private String name;
+    private Long price;
+
+}
