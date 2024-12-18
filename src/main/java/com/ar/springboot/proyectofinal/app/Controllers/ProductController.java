2d470981b224c7a2679e550a13ff0028bfdf2190
xXDarkayTberXx <madalolito22@gmail.com>
2024-12-18 13:57:03 +0100

    Cambiazos

--- src/main/java/com/ar/springboot/proyectofinal/app/Controllers/ProductController.java
+++ src/main/java/com/ar/springboot/proyectofinal/app/Controllers/ProductController.java
@@ -1,10 +1,67 @@
 package com.ar.springboot.proyectofinal.app.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.ar.springboot.proyectofinal.app.Model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
+
+    List<Product> productos = anyadirProductos();
+
+    @GetMapping("/products")
+    public String mostrarProductos(Model model) {
+
+        List<String> atributos = anyadirAntributos();
+
+        model.addAttribute("atributos", atributos);
+        model.addAttribute("productos", productos);
+
+        return "products";
+    }
+
+    @GetMapping("/formNewProduct")
+    public String formNewProduct(Model model) {
+        model.addAttribute("producto", new Product(null, null, null));
+        return "formNewProduct";
+    }
+
+    @PostMapping("/formPostNewProduct")
+    public void formNewProductPost(@ModelAttribute Product product) {
+        productos.add(product);
+    }
+
+    private List<Product> anyadirProductos() {
+        List<Product> productos= new ArrayList<Product>();
+
+        productos.add(new Product(1L, "Mesa Circular Razor", 200L));
+        productos.add(new Product(2L, "Módulo televisor Sinair", 850L));
+        productos.add(new Product(3L, "Silla metálica Candy", 180L));
+        productos.add(new Product(4L, "Sillón chaiselonge Roma", 1200L));
     
-    
-    
+        return productos;
+    }
+
+    private List<String> anyadirAntributos() {
+
+        List<String> atributos = new ArrayList<>();
+
+        atributos.add("Id");
+        atributos.add("Nombre");
+        atributos.add("Tipo de Producto");
+        atributos.add("Precio");
+        atributos.add("Precio Con IVA");
+        atributos.add("Acciones");
+
+        return atributos;
+
+    }
+
 }
--- src/main/java/com/ar/springboot/proyectofinal/app/Model/Product.java
+++ src/main/java/com/ar/springboot/proyectofinal/app/Model/Product.java
@@ -2,12 +2,23 @@ package com.ar.springboot.proyectofinal.app.Model;
 
 public class Product {
    
+    public Product() {
+    }
+
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        priceWithTaxes = priceWithTaxes();
+    }
+
     private Long id;
     private String name;
     private Long price;
+    private Long priceWithTaxes;
 
-    public Long priceWithTaxes(){
-        return 2L;
+    private Long priceWithTaxes(){
+        return (long)(this.price * 1.21);
     }
 
 }
